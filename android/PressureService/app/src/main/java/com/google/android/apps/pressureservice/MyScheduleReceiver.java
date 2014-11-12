21204973e83953736c8128e76c90d1a431ccb951
David Konerding <dakoner@gmail.com>
2014-11-12 13:51:15 -0800

    add a BroadcastReceiver that should start the app on boot.

---
+++ android/PressureService/app/src/main/java/com/google/android/apps/pressureservice/MyScheduleReceiver.java
@@ -0,0 +1,14 @@
+package com.google.android.apps.pressureservice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyScheduleReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // assumes WordService is a registered service
+        Intent intent2 = new Intent(context, LocalService.class);
+        context.startService(intent2);
+    }
+}
